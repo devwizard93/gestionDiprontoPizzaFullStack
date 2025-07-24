7f81a63d49ea4842e4af6e97de5bfb912d92c02d
Roberto <robertybenja3119@gmail.com>
2025-07-23 21:35:51 -0400

    backend + fronted angular servido

--- src/main/java/com/miempresa/miproyecto/model/Sale.java
+++ src/main/java/com/miempresa/miproyecto/model/Sale.java
@@ -1,31 +1,29 @@
-    package com.miempresa.miproyecto.model;
+package com.miempresa.miproyecto.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
-    import jakarta.persistence.*;
-    import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.TimeZone;
 
-    import java.time.LocalDateTime;
-    import java.util.List;
+@Entity
+@Data
+public class Sale {
 
-    @Entity
-    @Data
-    public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
 
+    private LocalDateTime startDate;
+    @ElementCollection
+    private List<String> productList;
+    private Double price;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long Id;
-
-        private LocalDateTime startDate;
-        @ElementCollection
-        private List<String> productList;
-        private Double price;
-        @PrePersist
-        public void prePersist() {
-            this.startDate = LocalDateTime.now();
-        }
-
-
-
-
+    @PrePersist
+    public void prePersist() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Santiago"));
     }
+
+
+}
